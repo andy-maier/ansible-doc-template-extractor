62ee0090b2b2b557dc9b7b10fbea0f07ec894f31
Andreas Maier <andy-maier@users.noreply.github.com>
2025-10-30 22:08:21 +0100

    Updated version file; Added dummy step to test workflow (#3)
    * Updated version file
    
    Signed-off-by: Andreas Maier <andreas.r.maier@gmx.de>
    
    * Added dummy step to define GitHub tests that must pass
    
    Signed-off-by: Andreas Maier <andreas.r.maier@gmx.de>
    
    ---------
    
    Signed-off-by: Andreas Maier <andreas.r.maier@gmx.de>

--- src/ansible_doc_template_extractor/_version_scm.py
+++ src/ansible_doc_template_extractor/_version_scm.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.dev0+d20251030'
-__version_tuple__ = version_tuple = (0, 1, 'dev0', 'd20251030')
+__version__ = version = '0.5.0a1.dev2+g292863cd7.d20251030'
+__version_tuple__ = version_tuple = (0, 5, 0, 'a1', 'dev2', 'g292863cd7.d20251030')
 
-__commit_id__ = commit_id = None
+__commit_id__ = commit_id = 'g292863cd7'
